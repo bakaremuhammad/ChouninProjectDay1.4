fa8b013f8aa7bafe488936d202f811d4ce7c8143
bakaremuhammad <thepsychometric09@gmail.com>
2024-05-22 01:30:45 +0100

    Changes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# input() will get user input in console
-# Then print() will print the word "Hello" and the user input
-print("Hello " + input("What is your name?"))
+a=input("a:")
+b=input("b:")
+c=a
+a=b
+b=c
+print("a=" + a)
+print("b=" + b)
